136befadfbb464cee5b6378039defbd4c3c12519
KaylaStiver <KaylaStiver3@gmail.com>
2022-04-28 08:49:03 -0400

    Added function template for doubles and floats

--- Unit 8 Assignment 2 B/Source.cpp
+++ Unit 8 Assignment 2 B/Source.cpp
@@ -2,9 +2,25 @@
 // CIS 1202 800
 // April 28th 2022
 
+#include <iostream>
+
 using namespace std;
 
+template <typename H>
+
+H half(H num) {
+	return (num / 2);
+}
+
 int main() {
+
+	int a = 3;
+	float b = 7.5f;
+	double c = 6.2;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
 
 	return 0;
 }
